b61f7a7dfdd96f81f28fc6c77a38842e53b78274
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2025-09-06 15:17:41 -0400

    Refactor header component and enhance styles for improved layout and responsiveness. Introduced new header structure with visual elements, updated CSS for better mobile support, and added accessibility features.

--- src/SharedModules/Components/Header/Header.jsx
+++ src/SharedModules/Components/Header/Header.jsx
@@ -1,30 +1,32 @@
 import React from 'react'
 import bg from '../../../assets/imgs/bg.png'
-export default function Header({title,paragraph}) {
-  return (
-    <>
-   
-    <div className='header-content text-white '> 
-    <div className="container-fluid">
-      <div className="row px-4 py-2 g-0 align-items-center">
-        <div className="col-sm-10">
-        <h3 className='ps-3'>{title}</h3>
-        <p className='ps-3'>{paragraph}</p>
-        </div>
-        <div className="col-md-2">
-        <img  className='img-fluid' src={bg} alt="background" />
-      </div>
-      </div>
 
-    </div>
-    
-   
-     
-   
-   
-    </div>
-    
-   
-    </>
+export default function Header({ title, paragraph }) {
+  return (
+    <header className='header-content text-white' role="banner">
+      <div className="container-fluid">
+        <div className="row g-0 align-items-center">
+          {/* Main Content Section */}
+          <div className="col-lg-10 col-md-9 col-sm-8">
+            <div className="header-text-content">
+              <h1 className="header-title">{title}</h1>
+              <p className="header-description">{paragraph}</p>
+            </div>
+          </div>
+          
+          {/* Visual Element Section */}
+          <div className="col-lg-2 col-md-3 col-sm-4">
+            <div className="header-visual-wrapper">
+              <img 
+                className='header-image' 
+                src={bg} 
+                alt="Decorative illustration representing the application theme" 
+                loading="lazy"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
